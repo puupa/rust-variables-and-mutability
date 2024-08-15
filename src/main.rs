743644b630f16281984a5a04a6143d942f2528f2
Puupa <arthur.van.acker@gmail.com>
2024-08-15 17:08:28 +0100

    add constants and shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    // Variables and Mutability
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+
+    // Constants
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("The value of THREE_HOURS_IN_SECONDS is: {THREE_HOURS_IN_SECONDS}");
+
+    // Shadowing
+    let y: i32 = 5;
+
+    let y: i32 = y + 1;
+
+    {
+        let y: i32 = y * 2;
+        println!("The value of y in the inner scope is: {y}");
+    }
+
+    println!("The value of y is: {y}");
 }
